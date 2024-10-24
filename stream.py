e28e55fc5327d567bd0704141fb3d50ba8566cb0
suresh-cra <90632329+suresh-cra@users.noreply.github.com>
2024-10-24 19:25:07 +0530

    Add files via upload

--- stream.py
+++ stream.py
@@ -1,3 +1,13 @@
-import streamlit as st
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
-st.write ("Hello Streamlit")
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
